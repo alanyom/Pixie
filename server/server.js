4092aa16c050d3ad5e9a26fc01d590b3ebeadce7
Alan Yom <89889751+alanyom@users.noreply.github.com>
2026-04-15 21:41:13 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json()); 
-
+ 
 // --- MOCK ROUTING ENGINE ---
 // This is where your 'Phase 1: US -> Pakistan' logic lives
 app.get('/api/quote', (req, res) => {
