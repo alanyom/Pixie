e56efbaf6690e56ff64b912278e98cb745cedea5
Alan Yom <89889751+alanyom@users.noreply.github.com>
2026-04-15 21:41:04 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
